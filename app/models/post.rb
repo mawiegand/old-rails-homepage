510bc6904f00b35d6ff9076a00085e9d0a3c8ca4
Marcel Wiegand <marcel@bluepanel.org>
2013-05-22 02:34:36 +0200

    first implementation of post tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,15 @@
 class Post < ActiveRecord::Base
   resourcify
 
-  attr_accessible :content, :title, :user_id
+  attr_accessible :content, :title, :user_id, :tags_attributes
 
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
   
   belongs_to :user, :inverse_of => :posts
   has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } } 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514200446) do
+ActiveRecord::Schema.define(:version => 20130522001107) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(:version => 20130514200446) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
